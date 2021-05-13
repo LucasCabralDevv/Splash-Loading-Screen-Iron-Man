deff6202e0a67b2e64006bf1f71b34d0833771c9
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-05-13 15:46:39 -0300

    Done

---
+++ app/src/main/java/com/lucascabral/ironmansplashscreen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lucascabral.ironmansplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+    }
+}
