c800c691f03945f3594e62bd05399a90ad65d5d2
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-01 14:43:08 -0300

    Done. 2

--- app/src/main/java/com/lucascabral/ironmansplashscreen/MainActivity.kt
+++ app/src/main/java/com/lucascabral/ironmansplashscreen/MainActivity.kt
@@ -3,11 +3,18 @@ package com.lucascabral.ironmansplashscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        val ironManAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.iron_man_anim)
+
+        splashIronMan.animation = ironManAnim
     }
 }
